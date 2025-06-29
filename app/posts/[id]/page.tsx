31920e885b249412fca23371748141e57421eaf3
Hanan3amer <hananamer312003@gmail.com>
2025-06-29 15:09:55 +0300

    fix in details page

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,21 +1,23 @@
 import { getPostByID } from "../../lib/api";
 import { Post } from "../../types/type";
-import { notFound } from "next/navigation";
 
-interface Props {
-  params: {
-    id: string;
-  };
-}
+type PostDetailsPageProps = {
+  params: { id: string };
+};
 
-export default async function PostDetailsPage({ params }: Props) {
+export default async function PostDetailsPage({
+  params,
+}: PostDetailsPageProps) {
   const post: Post = await getPostByID(params.id);
-
-  if (!post) return notFound();
 
   return (
     <div className="flex mx-auto items-center justify-center h-lvh">
-      <div className="block max-w-sm p-6 bg-white border border-amber-300 rounded-lg shadow-xs hover:bg-gray-100 dark:bg-gray-800 dark:border-purple-300 dark:hover:bg-gray-700">
+      <div
+        className="block max-w-sm p-6 border border-amber-300 dark:border-purple-300 rounded-lg shadow-xs hover:opacity-95 
+             bg-gradient-to-br from-white via-amber-50 to-yellow-100 
+             dark:bg-gradient-to-br dark:from-[#10101a] dark:via-[#1a1a2a] dark:to-[#2c2c3a]"
+        data-aos="fade-up"
+      >
         <h5 className="mb-2 text-md font-semibold text-gray-900 dark:text-white">
           {post.title}
         </h5>
