239c939138b697293d875f38fa87e3884f39f5cb
Hanan3amer <hananamer312003@gmail.com>
2025-06-29 21:11:36 +0300

    change in details  page

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,13 +1,21 @@
-import { notFound } from "next/navigation";
-export async function getAllPosts() {
+import { Post } from "@/app/types/type";
+
+export async function getAllPosts(): Promise<Post[]> {
   const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if (!data.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+
   return data.json();
 }
 
-export async function getPostByID(id: string) {
+export async function getPostByID(id: string): Promise<Post | null> {
   const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
   if (!data.ok) {
-    notFound();
+    return null;
   }
+
   return data.json();
 }
--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,19 +1,29 @@
-import { getPostByID } from "@/app/lib/api";
+import { getPostByID, getAllPosts } from "@/app/lib/api";
 import { Post } from "@/app/types/type";
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  const posts = await getAllPosts();
+  return posts.map((post: Post) => ({
+    id: post.id.toString(),
+  }));
+}
+
 export default async function PostDetailsPage({
   params,
 }: {
   params: { id: string };
 }) {
-  const post: Post = await getPostByID(params.id);
+  const post = await getPostByID(params.id);
+
+  if (!post) notFound(); 
 
   return (
     <div className="flex mx-auto items-center justify-center h-lvh">
       <div
         className="block max-w-sm p-6 border border-amber-300 dark:border-purple-300 rounded-lg shadow-xs hover:opacity-95 
-             bg-gradient-to-br from-white via-amber-50 to-yellow-100 
-             dark:bg-gradient-to-br dark:from-[#10101a] dark:via-[#1a1a2a] dark:to-[#2c2c3a]"
-        data-aos="fade-up"
+        bg-gradient-to-br from-white via-amber-50 to-yellow-100 
+        dark:bg-gradient-to-br dark:from-[#10101a] dark:via-[#1a1a2a] dark:to-[#2c2c3a]"
       >
         <h5 className="mb-2 text-md font-semibold text-gray-900 dark:text-white">
           {post.title}
