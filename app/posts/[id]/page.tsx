4b4f66618d375700c78599b6e0db67cc3de3a76e
Hanan3amer <hananamer312003@gmail.com>
2025-06-29 15:03:23 +0300

    fix type

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,20 +1,21 @@
 import { getPostByID } from "../../lib/api";
-type Props = {
+import { Post } from "../../types/type";
+import { notFound } from "next/navigation";
+
+interface Props {
   params: {
     id: string;
   };
-};
+}
 
 export default async function PostDetailsPage({ params }: Props) {
-  const post = await getPostByID(params.id);
+  const post: Post = await getPostByID(params.id);
+
+  if (!post) return notFound();
+
   return (
     <div className="flex mx-auto items-center justify-center h-lvh">
-      <div
-        className="block max-w-sm p-6 border border-amber-300 dark:border-purple-300 rounded-lg shadow-xs hover:opacity-95 
-             bg-gradient-to-br from-white via-amber-50 to-yellow-100 
-             dark:bg-gradient-to-br dark:from-[#10101a] dark:via-[#1a1a2a] dark:to-[#2c2c3a]"
-        data-aos="fade-up"
-      >
+      <div className="block max-w-sm p-6 bg-white border border-amber-300 rounded-lg shadow-xs hover:bg-gray-100 dark:bg-gray-800 dark:border-purple-300 dark:hover:bg-gray-700">
         <h5 className="mb-2 text-md font-semibold text-gray-900 dark:text-white">
           {post.title}
         </h5>
